f38caa6ddd0e5d8fde16f60ce7e795a9083af9d1
Raúl Silva <raul.silva@contpaqi.com>
2019-10-04 11:44:12 -0500

    Add project files.

---
+++ NLogRabbitMQClient/App.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Demo.NLogApp.RabbitMQ
+{
+    public class App
+    {
+        private readonly ITestService _testService;
+        private readonly ILogger<App> _logger;
+ 
+        public App(ITestService testService,
+            ILogger<App> logger)
+        {
+            _testService = testService;
+            _logger = logger;
+        }
+ 
+        public void Run()
+        {
+            _logger.LogInformation("This is a console application for {_config.Title}");
+            _testService.Run();
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ NLogRabbitMQClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+
+namespace Demo.NLogApp.RabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            var serviceCollection = new ServiceCollection();
+
+            ConfigureServices(configuration, serviceCollection);
+
+            // create service provider
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            // entry to run app
+            serviceProvider.GetService<App>().Run();
+
+
+        }
+
+        private static void ConfigureServices(IConfiguration config, IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.ClearProviders();
+                loggingBuilder.AddNLog(config);
+            });
+
+            serviceCollection.AddTransient<ITestService, TestService>();
+            serviceCollection.AddTransient<App>();
+        }
+    }
+}
